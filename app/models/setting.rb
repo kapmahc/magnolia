07c301a0e98dd7b1e29bdfceb9639432869cfbd2
Jitang Zheng <jitang.zheng@gmail.com>
2016-11-21 21:00:11 -0800

    fix layout

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,9 @@
 # RailsSettings Model
 class Setting < RailsSettings::Base
-  source Rails.root.join("config/app.yml")
+  source Rails.root.join('config/app.yml')
   namespace Rails.env
+
+  def self.site_info(k)
+    self["#{I18n.locale}://site/#{k}"]
+  end
 end
