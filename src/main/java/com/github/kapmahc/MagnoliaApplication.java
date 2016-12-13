45e7680178ea3f4f70db006272534a680ca63069
Jitang Zheng <jitang.zheng@gmail.com>
2016-12-13 13:10:50 -0800

    create project

---
+++ src/main/java/com/github/kapmahc/MagnoliaApplication.java
@@ -0,0 +1,12 @@
+package com.github.kapmahc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnoliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnoliaApplication.class, args);
+	}
+}
