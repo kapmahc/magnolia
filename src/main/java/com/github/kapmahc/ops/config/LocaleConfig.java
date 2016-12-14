30aeb5dded4e20f8568d768eeb53adba44c89e2f
Jitang Zheng <jitang.zheng@gmail.com>
2016-12-14 00:06:40 -0800

    i18n

--- src/main/java/com/github/kapmahc/ops/config/LocaleConfig.java
+++ src/main/java/com/github/kapmahc/ops/config/LocaleConfig.java
@@ -1,16 +1,27 @@
 package com.github.kapmahc.ops.config;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.AbstractResourceBasedMessageSource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.resource.ContentVersionStrategy;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
@@ -20,7 +31,34 @@ import java.util.Locale;
  */
 @Configuration
 @EnableWebMvc
-public class LocaleConfig extends WebMvcConfigurerAdapter {
+public class LocaleConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding(ENCODING);
+        return resolver;
+    }
+
+    private ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("classpath:/themes/" + theme + "/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCacheable(true);
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addDialect(new LayoutDialect());
+        engine.setEnableSpringELCompiler(true);
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
 
     @Bean(name = "multipartResolver")
     public CommonsMultipartResolver getMultipartResolver() {
@@ -58,17 +96,27 @@ public class LocaleConfig extends WebMvcConfigurerAdapter {
         VersionResourceResolver vrr = new VersionResourceResolver().addVersionStrategy(new ContentVersionStrategy(), "/**");
 
         registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/themes/bootstrap4/static/")
+                .addResourceLocations("classpath:/themes/" + theme + "/static/")
                 .setCachePeriod(60 * 60 * 24 * 365) /* one year */
                 .resourceChain(true)
                 .addResolver(vrr);
     }
 
-    // fix "o.s.web.servlet.PageNotFound"
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
 
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    private ApplicationContext applicationContext;
     private final static String LOCALE = "locale";
+    private final static String ENCODING = "UTF-8";
+    @Value("${app.theme}")
+    protected String theme;
+
 }
